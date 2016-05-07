891424d4e13471d2964ad21e1456e964ca12d12b
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-05-07 17:01:24 -0600

    json:api mime types in initializer

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,8 +1,8 @@
-# api_mime_types = %W(
-#   application/vnd.api+json
-#   text/x-json
-#   application/json
-# )
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
 
 # Mime::Type.unregister :json
-# Mime::Type.register 'application/json', :json, api_mime_types
+Mime::Type.register 'application/json', :json, api_mime_types
