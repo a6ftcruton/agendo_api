a710714f26ec9bdfdafa88bba6e8daace4300348
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-05-10 08:04:12 -0600

    add relationship to serializers

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,5 +1,7 @@
 #configured to use json_api adapter - see config/initializers/ams
 
 class TagSerializer < ActiveModel::Serializer
+  has_many :todos
+
   attributes :id, :name
 end
--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -1,5 +1,7 @@
 #configured to use json_api adapter - see config/initializers/ams
 
 class TodoSerializer < ActiveModel::Serializer
+  has_many :tags
+
   attributes :id, :title, :complete
 end
