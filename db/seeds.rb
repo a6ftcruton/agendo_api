bd5ae3c4c4b57607c7b8a3fa3ae7b780b2c35c20
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-05-30 13:58:40 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-todo1 = Todo.create({title: "First todo"})
-todo2 = Todo.create({title: "Next todo"})
+todo1 = Todo.create({title: "Do the dishes"})
+todo2 = Todo.create({title: "Organize camping gear"})
+todo3 = Todo.create({title: "Go for a hike"})
+todo4 = Todo.create({title: "Untagged todo"})
 
 tag1 = Tag.create({name: "Chores"})
 tag2 = Tag.create({name: "This weekend"})
 
 Tagging.create({todo: todo1, tag: tag1})
+Tagging.create({todo: todo2, tag: tag1})
 Tagging.create({todo: todo2, tag: tag2})
+Tagging.create({todo: todo3, tag: tag2})
