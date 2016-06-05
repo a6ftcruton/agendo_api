63fc5787a6d2785cbf82cf67697c538a5b6f12dc
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-06-05 15:21:52 -0600

    remove logging junk

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::TodosController < Api::V1::BaseController
-  # before_filter :massage_params_for_jsonapi, only: %i(update)
   before_filter :log_request, only: %i(index show create update destroy)
   before_filter :find_todos, only: %i(index)
   before_filter :find_todo, only: %i(show update destroy)
@@ -43,17 +42,6 @@ class Api::V1::TodosController < Api::V1::BaseController
   end
 
   private
-  # def massage_params_for_jsonapi
-  #   tagKey = params[:data][:relationships][:data]
-  #   puts "\n"
-  #   puts "*" * 40
-  #   puts "tagKey"
-  #   puts tagKey
-  #   puts "*" * 40
-  #   puts "\n"
-  #   tagKey = [] if tagKey.nil?
-  # end
-
   def log_request
     puts "\n"
     puts "*" * 40
@@ -78,10 +66,10 @@ class Api::V1::TodosController < Api::V1::BaseController
   end
 
   def todo_update_params
-    # puts "\n\nparams === #{params.inspect}"
-    # params.require(:data)
-    #       .permit(:relationships)
-    #       .permit(:attributes)
+    puts "\n"; puts "*" * 40
+    puts "ActiveModelSerializers::Deserialization.jsonapi_parse!(params)"
+    puts ActiveModelSerializers::Deserialization.jsonapi_parse!(params).inspect
+    puts "*" * 40; puts "\n" 
     ActiveModelSerializers::Deserialization.jsonapi_parse!(params)
   end
 end
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -6,13 +6,3 @@ api_mime_types = %W(
 
 # Mime::Type.unregister :json
 Mime::Type.register 'application/json', :json, api_mime_types
-
-parsers = Rails::VERSION::MAJOR >= 5 ? ActionDispatch::Http::Parameters : ActionDispatch::ParamsParser
-media_type = Mime::Type.lookup(ActiveModelSerializers::Jsonapi::MEDIA_TYPE)
-
-# Proposal: should actually deserialize the JSON API params
-# to the hash format expected by `ActiveModel::Serializers::JSON`
-# actionpack/lib/action_dispatch/http/parameters.rb
-parsers::DEFAULT_PARSERS[media_type] = lambda do |body|
-  data = JSON.parse(body)
-  data = { :_json => data } unless data.is_a?(Hash)
