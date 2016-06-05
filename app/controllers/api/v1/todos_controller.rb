afc11791a474dd38bb31f1615da470855cabf25c
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-06-05 12:06:20 -0600

    serialized params logging

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -66,8 +66,16 @@ class Api::V1::TodosController < Api::V1::BaseController
   end
 
   def todo_update_params
+     parsed = ActiveModel::Serializer::Adapter::JsonApi::Deserialization.parse(params.to_h)
+     puts "\n"
+     puts "*" * 40
+     puts "~~~~~parsed~~~~~ = #{parsed.inspect}"
+     puts "*" * 40
+     puts "\n"
+
     puts "\n\nparams === #{params.inspect}"
     params.require(:data)
-          .permit(:relationships, :attributes, :title, :complete)
+          .permit(:relationships)
+          .permit(:attributes)
   end
 end
