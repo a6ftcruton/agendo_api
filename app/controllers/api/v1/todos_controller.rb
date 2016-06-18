695012a72f6e979c73e74c435aca541c17bd146b
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-06-18 10:19:06 -0600

    workaround for deep_munge

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::TodosController < Api::V1::BaseController
+  before_filter :prevent_munge_error, only: %i(create update destroy)
   before_filter :log_request, only: %i(index show create update destroy)
   before_filter :find_todos, only: %i(index)
   before_filter :find_todo, only: %i(show update destroy)
@@ -51,12 +52,21 @@ class Api::V1::TodosController < Api::V1::BaseController
     puts "\n"
   end
 
+  def prevent_munge_error
+    #in config.action_dispatch.perform_deep_munge = false  not working
+    params[:data][:relationships][:tags][:data] ||= []
+  end
+
   def find_todos
     @todos = Todo.all 
   end
 
   def find_todo
     @todo = Todo.find_by(id: params[:id])
+    puts "\n"; puts "*" * 40
+    puts "----find_todo---"
+    puts "@todo"; puts @todo.inspect
+    puts "*" * 40; puts "\n"
   end
 
   def todo_params
